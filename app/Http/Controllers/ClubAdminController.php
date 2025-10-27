d4f2905667a565bf390b87885c64923797afb779
Adam <adametyang69@icloud.com>
2025-10-27 12:06:37 +0300

    ClubAdmin

--- app/Http/Controllers/ClubAdminController.php
+++ app/Http/Controllers/ClubAdminController.php
@@ -13,17 +13,71 @@ class ClubAdminController extends Controller
     public function dashboard(Club $club)
     {
         return inertia('ClubAdmin/Dashboard', [
-
+            'club' => $club->load([
+            'members' => function ($q) {
+                $q->with('adminRecords');
+            },
+            'events'
+            ]),
+            'members' => $club->members()->count(),
+            'events' => $club->events()->count(),
+            'upcomingEvents' => $club
+                                ->events()
+                                ->where('start_date', '>', now())
+                                ->get(),
+            'admins' => $club->admins()->count(),
         ]);
     }
 
     public function indexMembers(Club $club)
     {
-        return inertia('', [
+        $this->authorize('manageClub', $club);
+        return inertia('ClubAdmin/Members/Index', [
+        'club'=>$club->load('members'),
+        'members'=>$club->members,
         
         ]);
     }
-    public function updateMemberRole(Request $request,Student $student, )
+    
+    public function updateMemberRole(Request $request,Club $club, Student $student)
     {
+        
+        return redirect()->back()->with();
     }
+    
+    public function removeMember(Club $club, Student $student)
+    {
+        return redirect();
+    }
+    
+    public function indexEvents (Club $club)
+    {
+        return inertia('ClubAdmin/Events/Index', []);
+    }
+    
+    public function showEvent(Club $club, Event $event)
+    {
+        
+        return inertia('ClubAdmin/Events/Show', [
+            'club'=>$club,
+            'event' => $event,
+            'attendanceCount' =>$event->atendees()->count(),
+            'atendees'=> $event->atendees(),
+        ]);
+    }
+    
+    public function editClub(Club $club)
+    {
+        $this->authorize('manageClub', $club);
+
+        return inertia('ClubAdmin/Club/Edit', [
+            'club' => $club,
+        ]);
+    }
+    
+    public function updateClub(Request $request, Club $club)
+    {
+        $this->authorize('manageClub', $club);
+    }
+    
 }
