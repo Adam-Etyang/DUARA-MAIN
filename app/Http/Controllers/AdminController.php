d5bbb6df4c1321f3feb7435287629a56d2c90516
Adam <adametyang69@icloud.com>
2025-10-30 19:17:22 +0300

    I dont remember the change

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -34,7 +34,7 @@ class AdminController extends Controller
 
     public function indexClubs()
     {
-        return inertia('Admin/Clubs/Index', [
+        return inertia('Admin/Club/Index', [
             'clubs' => Club::with('members')
                             ->withCount('members')
                             ->latest()
